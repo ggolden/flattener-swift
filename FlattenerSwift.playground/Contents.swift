5594c9a54cbba88caccdacc0758d5e8ca0549727
Glenn R. Golden <ggolden@etudes.org>
2017-04-01 16:11:35 -0400

    initial commit

---
+++ FlattenerSwift.playground/Contents.swift
@@ -0,0 +1,123 @@
+//
+//  FlattenerSwift.playground
+//
+//  Copyright 2017 Glenn R. Golden
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//  for a clue as to what this is, see https://github.com/ggolden/flattener-swift
+//
+
+import XCTest
+
+// flatten the array of arbitrary mix of objects and similar arrays into a single array of objects
+func flatten(_ array: [Any]) -> [Any]
+{
+	// accumulate the result here
+	var rv: [Any] = []
+	
+	// process each item
+	for item in array
+	{
+		// when we run into an array, recurse on it, appending the results
+		if let itemAsArray = item as? [Any]
+		{
+			rv.append(contentsOf: flatten(itemAsArray))
+		}
+			
+			// when we run into a non-array item, simply append it
+		else
+		{
+			rv.append(item)
+		}
+	}
+	
+	// here's what we got
+	return rv
+}
+
+class FlattenerTests: XCTestCase
+{
+	func testFlaten()
+	{
+		var target: [Any] = []
+		var sample: [Any] = []
+		var flattened = flatten(sample)
+		XCTAssertTrue(isEqualArray(Int.self, flattened, target)!, "\(sample) -> \(flattened)")
+		
+		target = [1,2,3,4,5,6,7]
+		sample = [1,2,3,4,5,6,7]
+		flattened = flatten(sample)
+		XCTAssertTrue(isEqualArray(Int.self, flattened, target)!, "\(sample) -> \(flattened)")
+		
+		target = [1,2,3,4,5,6,7]
+		sample = [[1],[2],[3],[4],[5],[6],[7]]
+		flattened = flatten(sample)
+		XCTAssertTrue(isEqualArray(Int.self, flattened, target)!, "\(sample) -> \(flattened)")
+		
+		target = [1,2,3,4,5,6,7]
+		sample = [[1,2],[3,4],[5,6],[7]]
+		flattened = flatten(sample)
+		XCTAssertTrue(isEqualArray(Int.self, flattened, target)!, "\(sample) -> \(flattened)")
+		
+		target = [1,2,3,4,5,6,7]
+		sample = [[1,2,3,4],[5,6],7]
+		flattened = flatten(sample)
+		XCTAssertTrue(isEqualArray(Int.self, flattened, target)!, "\(sample) -> \(flattened)")
+		
+		target = [1,2,3,4]
+		sample = [[1,2,3],4]
+		flattened = flatten(sample)
+		XCTAssertTrue(isEqualArray(Int.self, flattened, target)!, "\(sample) -> \(flattened)")
+		
+		target = [1,2,3,4]
+		sample = [[1,2,[3]],4]
+		flattened = flatten(sample)
+		XCTAssertTrue(isEqualArray(Int.self, flattened, target)!, "\(sample) -> \(flattened)")
+		
+		target = [1,2,3,4]
+		sample = [[1,[[2]],[3]],4]
+		flattened = flatten(sample)
+		XCTAssertTrue(isEqualArray(Int.self, flattened, target)!, "\(sample) -> \(flattened)")
+	}
+	
+	// helper for the test, to compare two arrays of any single type
+	func isEqualArray<T: Equatable>(_ type: T.Type, _ a: [Any], _ b: [Any]) -> Bool?
+	{
+		if (a.count != b.count)
+		{
+			return false
+		}
+		for i in 0 ..< a.count
+		{
+			if let aItem = a[i] as? T, let bItem = b[i] as? T
+			{
+				if aItem != bItem
+				{
+					return false
+				}
+			}
+			else
+			{
+				return false
+			}
+		}
+		
+		return true
+	}
+}
+
+// run the tests
+let test = FlattenerTests.defaultTestSuite()
+test.run()
+
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # flattener-swift
-flatten arbitrairly embedded arrays into a flat array, in swift
+Flatten arbitrarily embedded arrays into a flat array, in Swift.
+
+Inspired by the [Nutshell Developer Take Home Project](https://github.com/nutshellcrm/join-the-team/blob/master/developer-questions.md#flattener) 
+
+This sample shows how to create an array of values that is the flattened version of an array that may contain values mixed with arrays of the same type.  For example:
+
+`[[1,[[2]],[3]],4]`
+
+### To Run
+
+This code is a Swift playground.  I made it for Mac, to avoid needing to run an IOS emulator.
+
+Download it on a Mac with XCode, and when you open it, it will run the tests.
+
+A sample run result is:
+
+```
+Test Suite 'FlattenerTests' started at 2017-04-01 16:06:44.485
+Test Case '-[__lldb_expr_1.FlattenerTests testFlaten]' started.
+Test Case '-[__lldb_expr_1.FlattenerTests testFlaten]' passed (0.095 seconds).
+Test Suite 'FlattenerTests' passed at 2017-04-01 16:06:44.581.
+	 Executed 1 test, with 0 failures (0 unexpected) in 0.095 (0.096) seconds
+```	 
+
+### Approach
+
+Recursion.  Modern languages (Swift, Java) have "flatten" methods on streams or arrays, but these would take a more uniform structure, such as an array of arrays of values, into an array of values.  They are not designed to handle the complexity of this data structure.  So we do a simple recursive method to unwrap the complexity into an array.
+
+### Comment
+
+This data structure is a challenge for strongly typed languages such as Java or Swift.  You can get around strong typing by using a general type that covers the items and Array, such as "Any" in Swift.
+
+More open languages like Javascript would easily handle the structure, but the implementation of flatten() could be the same.
+
+I don't know where one would use a data structure like this.  It seems a bit contrived.  But it leads to a fun little demonstration of recursion.
